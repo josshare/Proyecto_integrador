0fcfed7a7d32ae5a77d1233379ee5e14a278ef67
jose <josmtz.uber@gmail.com>
2024-08-18 17:55:54 -0500

    First Solution.

--- src/Persona.java
+++ src/Persona.java
@@ -1,3 +1,22 @@
-public interface Persona {
-    void getDetalles();
+public class Persona {
+    String nombre;
+    String apellido;
+    int edad;
+    String sexo;
+
+    public Persona() {}
+
+    public Persona(String nombre, String apellido, int edad, String sexo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.sexo = sexo;
+    }
+
+    public void getDetalles() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+        System.out.println("Edad: " + edad);
+        System.out.println("Sexo: " + sexo);
+    }
 }
